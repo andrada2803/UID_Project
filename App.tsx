8ebb61329398852a1cc524d01379edfeb6dc6f23
Ileana Pop <ileanapop1811@gmail.com>
2022-12-27 16:06:46 +0200

    chore: navigation setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/features/HomeScreen/HomeScreen';
+import TimetableScreen from './src/features/TimetableScreen/TimetableScreen';
+
+
+const MainStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="Home">
+        <MainStack.Screen name="Home" component={HomeScreen} />
+        <MainStack.Screen name="Timetable" component={TimetableScreen} />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/features/TimetableScreen/TimetableScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const TimetableScreen = () => {
+  return (
+    <Text>TimetableScreen</Text>
+  )
+}
+
+export default TimetableScreen
