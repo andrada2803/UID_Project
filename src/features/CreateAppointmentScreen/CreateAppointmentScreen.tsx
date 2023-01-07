5860bda9fc53f662956f052c838c362297ce4f38
SinMax <cosmin.stretea@gmail.com>
2023-01-07 23:26:13 +0200

    feat: some updates on create appointment, nu va suparatz pls

--- src/features/CreateAppointmentScreen/CreateAppointmentScreen.tsx
+++ src/features/CreateAppointmentScreen/CreateAppointmentScreen.tsx
@@ -1,128 +1,113 @@
 import React, { useState } from 'react';
-import { View, Alert , Button, StyleSheet, Text, Modal, Image } from 'react-native';
+import {
+    View,
+    Alert,
+    Button,
+    StyleSheet,
+    Text,
+    Modal,
+    Image,
+} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import DatePicker from 'react-native-modern-datepicker';
 
-export const CreateAppointmentScreen = () => {
+const confirmationScreenTitle = 'Appointment created successfully!';
+const confirmationScreenMessage =
+    'Check your email for the appointment confirmation';
 
+//@ts-ignore
+export const CreateAppointmentScreen = ({ navigation }) => {
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
     const [items, setItems] = useState([
-        {label: 'Student Certificate', value: 'sc'},
-        {label: 'Signing Student ID', value: 'sign'},
-        {label: 'Pick up documents', value: 'pud'},
-        {label: 'Freeze Year', value: 'fy'},
-        {label: 'Pay Taxes', value: 'pt'},
+        { label: 'Student Certificate', value: 'sc' },
+        { label: 'Signing Student ID', value: 'sign' },
+        { label: 'Pick up documents', value: 'pud' },
+        { label: 'Freeze Year', value: 'fy' },
+        { label: 'Pay Taxes', value: 'pt' },
     ]);
     const [selectedDate, setSelectedDate] = useState('');
-    const [isModalVisible, setModalVisible] = useState(false);
     const cuurentDate = new Date();
-    const minDate = cuurentDate.toISOString().substr(0,10)
+    const minDate = cuurentDate.toISOString().substr(0, 10);
 
     const handleSubmit = () => {
-        if(value === null) {
+        if (value === null) {
             Alert.alert('Error', 'Please select a reason');
-        }else if(selectedDate == ''){
+        } else if (selectedDate == '') {
             Alert.alert('Error', 'Please select a date');
-        }else{
-            setModalVisible(true)
+        } else {
+            navigation.navigate('ConfirmationScreen', {
+                title: confirmationScreenTitle,
+                message: confirmationScreenMessage,
+                noOfScreensToPop: 2,
+            });
         }
     };
 
     return (
         <View style={styles.container}>
-        <Text style={styles.text}>
-            Why would you like to make an appointment?
-        </Text>
-        <DropDownPicker
-            open={open}
-            value={value}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-            style={styles.dropdown}
-        />
-        <DatePicker
-            minimumDate={minDate}
-            maximumDate='2023-12-23'
-            style={styles.datePicker}
-            onSelectedChange={(date: React.SetStateAction<string>) => setSelectedDate(date)}/>
+            <Text style={styles.text}>
+                Why would you like to make an appointment?
+            </Text>
+            <DropDownPicker
+                open={open}
+                value={value}
+                items={items}
+                setOpen={setOpen}
+                setValue={setValue}
+                setItems={setItems}
+                style={styles.dropdown}
+            />
+            <DatePicker
+                minimumDate={minDate}
+                maximumDate='2023-12-23'
+                style={styles.datePicker}
+                onSelectedChange={(date: React.SetStateAction<string>) =>
+                    setSelectedDate(date)
+                }
+            />
 
-        <Text style={styles.selectedDate}>
-            {selectedDate}
-        </Text>
-        <View style={styles.submitButton}>
-                <Button title="Submit" onPress={handleSubmit}/>
-
-                <Modal
-                    visible={isModalVisible}
-                    onRequestClose={() => setModalVisible(false)}
-                >
-                    <View style={{ top: 50, alignItems: 'center' }}>
-                        <Image source={require('../../assets/Confirmation.png')} style={styles.image}/>
-                        
-                        <View style={styles.textView}> 
-
-                            <Text style={styles.successText}>
-                                Appointment created successfully!
-                            </Text>
-
-                            <Text style={styles.infoText}>
-                                Check your email for the appointment confirmation
-                            </Text>
-
-                        </View>
-
-                        <View style={styles.buttonLayout}>
-                            <Button
-                                title="GO BACK"
-                                onPress={() => setModalVisible(false)}
-                            />
-                        </View>
-                    </View>
-                </Modal>
+            <Text style={styles.selectedDate}>{selectedDate}</Text>
+            <View style={styles.submitButton}>
+                <Button title='Submit' onPress={handleSubmit} />
             </View>
-
         </View>
-        
-    )
-  }
-  
-  const styles = StyleSheet.create({
+    );
+};
 
+const styles = StyleSheet.create({
     container: {
         flexDirection: 'column',
         flex: 1,
-        backgroundColor: "white",
-        padding: 20
+        backgroundColor: 'white',
+        padding: 20,
     },
 
     text: {
         fontFamily: 'Inter',
         fontSize: 20,
         bottom: 10,
-        top: 10
+        top: 10,
     },
 
     dropdown: {
-        top: 20
+        top: 20,
     },
 
     submitButton: {
         flex: 1,
         //bottom: 10,
         justifyContent: 'flex-end',
-    }, 
+    },
 
     image: {
         //IDKKKK
-        top: 160
+        top: 160,
     },
 
     buttonLayout: {
         //IDKKKK
-        bottom: -290
+        bottom: -290,
     },
 
     successText: {
@@ -137,22 +122,19 @@ export const CreateAppointmentScreen = () => {
         fontFamily: 'Inter',
         textAlign: 'center',
         fontSize: 20,
-        padding: 50
+        padding: 50,
     },
 
     textView: {
         //IDKKKK
-        top: 220
+        top: 220,
     },
 
     datePicker: {
-      top: 60  
+        top: 60,
     },
     selectedDate: {
         top: 40,
-        alignSelf: 'center'
-    }
-
-  
-  });
-  
+        alignSelf: 'center',
+    },
+});
